756c97d93a503cb7326b1f0e016d5a94d865181d
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2018-10-25 22:00:55 +0200

    create component structure

---
+++ src/app/_components/chat/contact-profile/contact-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-profile',
+  templateUrl: './contact-profile.component.html',
+  styleUrls: ['./contact-profile.component.scss']
+})
+export class ContactProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
