ff6d24fff268af953f187fc234587886e3834500
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2018-10-25 19:39:27 +0200

    finish the structure of project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Chat App Evolvice';
+export class AppComponent implements OnInit {
   navLinks = [
     { path: '/home', icon: 'home', label: 'Home' },
     { path: '/map', icon: 'map', label: 'Map' },
     { path: '/chat', icon: 'chat', label: 'Chat' }
   ];
+  route: string;
+  constructor(private location: Location, private router: Router) {
+    this.router.events.subscribe(val => {
+      if (location.path() != '') {
+        const currentRoute = location.path();
+        this.route = currentRoute.replace('/', '').toUpperCase();
+      } else {
+        this.route = 'HOME';
+      }
+    });
+  }
+
+  ngOnInit() {}
 }
