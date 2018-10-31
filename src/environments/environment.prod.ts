493e10f27aa0da2e8043e232225bf4efbcafa704
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2018-10-31 21:45:51 +0200

    edit db

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  BASE_URL: 'http://localhost:3000',
+  googleMapApiKey: 'AIzaSyD1uXumOcpOZ5iLZXJe2lwEzn-NT0cw2tw',
+  googleMapsApiEndPoint: 'https://maps.googleapis.com/maps/api/geocode/json'
 };
